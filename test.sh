19c8989257200e351b91ce85b3236bc2e304fe21
Quynh Dang <qdang@veritone.com>
2018-06-19 08:17:46 -0700

    Back to  Channel

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-./vt-engine-tools merge-vlf samples/86940638_0_v-vlf.json samples/86940660_0_v-vlf.json --output res/output-channel.json --idTag channel
+./vt-engine-tools merge-vlf samples/86940638_0_v-vlf.json samples/86940660_0_v-vlf.json --output res/output-channel.json --idTag Channel
